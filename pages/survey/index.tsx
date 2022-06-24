9a671cf9f4dac71bdc8ead0b9396b98131165d0a
peter_alfie <alfie1104@gmail.com>
2022-06-24 14:22:52 +0900

    feat(survey/index): change answer sentence to emoji

--- pages/survey/index.tsx
+++ pages/survey/index.tsx
@@ -13,32 +13,28 @@ import { numberPicker } from "../../util/number-picker";
 const questions = [
   {
     id: 1,
-    question: "당신은 누구와 함께 걷고 있습니까?",
-    answers: ["가족", "친구"],
+    question: "잠을 충분히 주무셨나요?",
+    answers: ["😀", "🤔"],
   },
   {
     id: 2,
-    question:
-      "그 사람과 함께 숲속을 계속 걸어가다 보면 한 동물을 만나게 됩니다. 그 동물은 무엇인가요?",
-    answers: ["호랑이", "토끼"],
+    question: "건강한 음식을 드셨나요?",
+    answers: ["😀", "🤔"],
   },
   {
     id: 3,
-    question:
-      "숲속으로 더 깊이 걸어가다 보면 공터가 나옵니다. 그 중심에는 한 채의 집이 있습니다. 그 집에는 울타리가 있나요?",
-    answers: ["네", "아니오"],
+    question: "오늘 하늘을 보셨나요?",
+    answers: ["😀", "🤔"],
   },
   {
     id: 4,
-    question:
-      "집에 다가 가면 문이 조금 열려 있는 것이 보입니다. 살짝 문을 열고 들어가면 거기에는 테이블이 있습니다. 테이블에는 무엇이 있습니까?",
-    answers: ["총", "음식"],
+    question: "친구와 안부전화를 하셨나요?",
+    answers: ["😀", "🤔"],
   },
   {
     id: 5,
-    question:
-      "집안을 둘러본 당신은 뒷문으로 나가 보았습니다. 그곳에는 잔디가 펼쳐져 있고, 중심에는 정원이 있고, 물이 있습니다. 물에 얼굴을 비추어 봅니다. 얼굴은 무엇으로 보이나요?",
-    answers: ["내 얼굴", "낯선 얼굴 혹은 생명체"],
+    question: "지켜주고 싶은 사람을 떠올려 보셨나요?",
+    answers: ["😀", "🤔"],
   },
 ];
 
@@ -132,29 +128,24 @@ function SurveyPage() {
               {questions[questionInfo.questionId].question}
             </span>
           </div>
-          <div className="flex flex-col items-center justify-center w-full">
+          <div className="flex items-center justify-center w-full">
             {questions[questionInfo.questionId].answers.map((answer, index) => (
               <div
                 key={answer}
-                className={`max-w-[60%] min-w-[20%] cursor-pointer bg-blue-500 bg-opacity-20 border border-blue-800 rounded-sm mb-3 text-blue-800 font-semibold hover:bg-opacity-30 transition-all`}
+                className={`text-4xl mx-5 lg:mx-10 cursor-pointer font-semibold transition-all p-2 hover:bg-green-100 rounded-2xl ${
+                  answers[questionInfo.questionId] !== undefined &&
+                  answers[questionInfo.questionId] === index &&
+                  "bg-green-200 rounded-2xl"
+                }`}
                 onClick={(e: React.MouseEvent) =>
                   handleClickAnswer(e, questionInfo.questionId, index)
                 }
               >
-                <span
-                  className={`${
-                    answers[questionInfo.questionId] !== undefined &&
-                    answers[questionInfo.questionId] === index
-                      ? "bg-green-100"
-                      : "bg-gray-50"
-                  }  py-1 px-3 m-1 text-base inline-block border rounded-sm border-blue-800`}
-                >
-                  {index + 1}
-                </span>
-                <span>{answer}</span>
+                {answer}
               </div>
             ))}
           </div>
+
           {questionInfo.questionId === questions.length - 1 &&
             !answers.includes(undefined) && (
               <button
