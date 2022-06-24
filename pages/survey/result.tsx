c0d2dd0a66a7ea9109df22b58201da03188c9f3e
peter_alfie <alfie1104@gmail.com>
2022-06-24 16:05:51 +0900

    feat: modify motion effect on number

--- pages/survey/result.tsx
+++ pages/survey/result.tsx
@@ -14,9 +14,11 @@ const lineVariants: Variants = {
 const numberVariants: Variants = {
   hidden: {
     opacity: 0,
+    rotate: 30,
   },
   visible: {
     opacity: 1,
+    rotate: 0,
   },
 };
 
@@ -36,20 +38,18 @@ function SurveyResultPage() {
         <meta name="description" content="번호 생성 결과" />
       </Head>
       <div className="bg-gray-900 w-full h-[100vh] flex flex-col items-center justify-center">
-        <div className="text-white font-thin text-3xl text-center mb-5">
-          {parsedNumbers &&
-            parsedNumbers.map((number, index) => (
-              <motion.span
-                key={number}
-                className="mx-5"
-                initial="hidden"
-                animate="visible"
-                variants={numberVariants}
-                transition={{ duration: 0.5, delay: 4.5 + index * 0.5 }}
-              >
-                {number}
-              </motion.span>
-            ))}
+        <div className="text-white font-thin text-3xl text-center mb-5 flex items-center justify-center gap-7">
+          {parsedNumbers.map((number, index) => (
+            <motion.div
+              key={`number_${number}`}
+              initial="hidden"
+              animate="visible"
+              variants={numberVariants}
+              transition={{ duration: 0.5, delay: 4.5 + index * 0.5 }}
+            >
+              {number}
+            </motion.div>
+          ))}
         </div>
         <motion.hr
           variants={lineVariants}
